3a3ac0c0e335e5d227e2d18f636a962fe1ced4fb
MaratByd04 <143888006+MaratByd04@users.noreply.github.com>
2023-09-21 22:17:42 +0300

    Добавьте файлы проекта.

---
+++ tumakov.exercises_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tumakov.exercises_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 2.1.Написать программу, которая спрашивает имя пользователя, и затем приветствует пользователя по имени.\n");
+
+            Console.WriteLine("Введите свое имя.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Добро пожаловать, " +  userName);
+
+            Console.WriteLine("Упражнение 2. Деление одного числа на другое. \n");
+
+            Console.WriteLine("Введите два целых числа.");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double resultOfDivision = a / b;
+            if (a!=0 && b!=0)
+                {
+                Console.WriteLine(resultOfDivision);
+                }
+            Console.WriteLine("")
+            
+
+
+        }
+    }
+}
